7178b74d3b5e370249f880187ae0b93a53ce4e2f
ethan2 <116553314+rat-balls@users.noreply.github.com>
2023-01-03 16:07:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
+const bg = document.getElementById("bg");
+const btn = document.getElementById("btn")
+
+spaceList = []
+index = 0;
+l = false
+
 function start(){
     bg.classList.add("animatedBG")
     btn.remove()
     createGrid()
-    createAliens()
+    moveAliens()
 }
 
-function createAliens(){
-    for(let i = 0;i < 12; i++ ){
-        document.getElementById(`space${i}`).classList.add("alien")
-    }
+function moveAliens(){
+    setInterval(function(){
+        spaceList[index].classList.add("alien")
+        index = index + 1
+        if(spaceList[index-12].classList.contains("alien")){
+            spaceList[index-12].classList.remove("alien")
+        }
+        }, 1000)
 }
 
 function createGrid(){
         y = 0
         x = 0
-    for(let i = 0;i < 160; i++ ){
+    for(let i = 0;i < 240; i++ ){
         space = document.createElement("div")
         space.classList.add("space")
         space.setAttribute("id", `space${i}`)
         document.body.insertBefore(space, bg)
         document.getElementById(`space${i}`).style.left = `calc(20vw + ${50*x}px)`
         document.getElementById(`space${i}`).style.top = `calc(${50*y}px)`
-        x = x + 1
-        if(x >= 16){
-            x = 0
+        if(l == true){
+            x = x - 1
+        } else {
+            x = x + 1
+        }
+        if(x == 16){
+            l = true
+            y = y + 1
+        } else if(x == 0){
+            l = false
             y = y + 1
         }
+        console.log(x)
+        console.log(y)
+        console.log(l)
+        spaceList[i] = document.getElementById(`space${i}`)
     }
 }
