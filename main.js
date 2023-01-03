52f52ca172ecf51797fcbe49a1da09e8504e39ea
Ramjoh <romjah@orange.fr>
2023-01-03 15:18:55 +0100

    musique

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 function start(){
-    bg.classList.add("animatedBG")
-    btn.remove()
-    createGrid()
+    bg.classList.add("animatedBG");
+    btn.remove();
+    createGrid();
 }
 
 
 function createGrid(){
-        y = 0
-        x = 0
+        y = 0;
+        x = 0;
     for(let i = 0;i < 160; i++ ){
-        space = document.createElement("div")
-        space.classList.add("space")
-        space.setAttribute("id", `space${i}`)
-        document.body.insertBefore(space, bg)
-        document.getElementById(`space${i}`).style.left = `calc(20vw + ${50*x}px)`
-        document.getElementById(`space${i}`).style.top = `calc(${50*y}px)`
-        x = x + 1
+        space = document.createElement("div");
+        space.classList.add("space");
+        space.setAttribute("id", `space${i}`);
+        document.body.insertBefore(space, bg);
+        document.getElementById(`space${i}`).style.left = `calc(20vw + ${50*x}px)`;
+        document.getElementById(`space${i}`).style.top = `calc(${50*y}px)`;
+        x = x + 1;
         if(x >= 16){
-            x = 0
-            y = y + 1
+            x = 0;
+            y = y + 1;
         }
     }
+}
+function createVaisseau(){
+    x = 0;
+    y = 0;
+    
 }
