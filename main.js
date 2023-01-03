fdc913dbe0367e6316e86c61da709719f63b2418
ethan2 <116553314+rat-balls@users.noreply.github.com>
2023-01-03 16:48:48 +0100

    aliens moving

--- main.js
+++ main.js
@@ -2,48 +2,76 @@ const bg = document.getElementById("bg");
 const btn = document.getElementById("btn")
 
 spaceList = []
-index = 0;
-l = false
+
 
 function start(){
     bg.classList.add("animatedBG")
     btn.remove()
     createGrid()
-    moveAliens()
+    var index = 0;
+    var speed = 1000
+
+    moveAliens(speed)
+
+    setInterval(function(){
+        if((index%16) === 0){
+            console.log(index)
+            speed = speed * 0.3
+            console.log("The aliens are advancing faster!")
+        }
+    },1000)
 }
 
-function moveAliens(){
+function moveAliens(speed){
+    
+
     setInterval(function(){
         spaceList[index].classList.add("alien")
         index = index + 1
-        if(spaceList[index-12].classList.contains("alien")){
-            spaceList[index-12].classList.remove("alien")
+        console.log("Speed is " + speed)
+        if(spaceList[index-24].classList.contains("alien")){
+            spaceList[index-24].classList.remove("alien")
         }
-        }, 1000)
+        }, speed)
 }
 
 function createGrid(){
         y = 0
         x = 0
-    for(let i = 0;i < 240; i++ ){
+        o = 2
+        l = false
+        p = false
+    for(let i = 0;i < 320; i++ ){
         space = document.createElement("div")
         space.classList.add("space")
         space.setAttribute("id", `space${i}`)
         document.body.insertBefore(space, bg)
         document.getElementById(`space${i}`).style.left = `calc(20vw + ${50*x}px)`
         document.getElementById(`space${i}`).style.top = `calc(${50*y}px)`
-        if(l == true){
-            x = x - 1
-        } else {
-            x = x + 1
-        }
-        if(x == 16){
+        if(x > 16 && o == 0){
             l = true
+            o = 1
             y = y + 1
-        } else if(x == 0){
+        } else if(x < 0 && o == 0){
             l = false
+            o = 1
             y = y + 1
         }
+
+        if(l == true && o == 1){
+            x = x
+            o = 2
+        } else if (l == false && o == 1){
+            x = x
+            o = 2
+        } else if (l == false){
+            x = x + 1
+            o = 0
+        } else if (l == true){
+            x = x - 1
+            o = 0
+        }
+        console.log(o)
         console.log(x)
         console.log(y)
         console.log(l)
