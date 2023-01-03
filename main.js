376d3da28ef7895441aca6e5e72c253cd6a58f72
ethan2 <116553314+rat-balls@users.noreply.github.com>
2023-01-03 15:03:50 +0100

    created grid

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 function start(){
     bg.classList.add("animatedBG")
     btn.remove()
-    setTimeout(function(){
-        aliens.forEach(element => {
-            setInterval(function() {moveInvader(element)} , 1000)}, 1000);
-            iterations = iterations + 1
-        });
+    createGrid()
 }
 
 
 function createGrid(){
         y = 0
         x = 0
-    for(let i = 0;i < 120; i++ ){
+    for(let i = 0;i < 160; i++ ){
         space = document.createElement("div")
         space.classList.add("space")
         space.setAttribute("id", `space${i}`)
@@ -20,7 +16,7 @@ function createGrid(){
         document.getElementById(`space${i}`).style.left = `calc(20vw + ${50*x}px)`
         document.getElementById(`space${i}`).style.top = `calc(${50*y}px)`
         x = x + 1
-        if(x >= 12){
+        if(x >= 16){
             x = 0
             y = y + 1
         }
